4fc114ad96e6d1d534512544f7a67ad1bc1c7c12
swapin <swapinvidya360@gmail.com>
2021-10-06 17:00:40 +0530

    basic

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: rgb(241, 229, 229);
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+            .logo{
+                font-size: 80px;
+            }
+
+            .title {
+                font-size: 60px;
+            }
+
+            .subtitle {
+                font-size: 25px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 10px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    {LOGO}
+                </div>
+
+                <div class="title m-b-md">
+                    Appname
+                </div>
+
+                <div class="subtitle m-b-md">
+                    Social Media Management WebApp <br> Version 0.1
+                </div>    
+
+                <div class="links">
+                    <a href="#">Docs</a>
+                    <a href="#">Timeline</a>
+                    <a href="#">News</a>
+                    <a href="#">Blog</a>
+                    <a href="#">GitHub</a>
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
