a1e4cc62bcec34330474b678160cef8b14b875b6
swapin <swapinvidya360@gmail.com>
2021-12-17 21:25:03 +0530

    filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,10 +50,12 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => '../public_html',
+            'url' => env('APP_URL'),
             'visibility' => 'public',
         ],
+        
+    
 
         's3' => [
             'driver' => 's3',
